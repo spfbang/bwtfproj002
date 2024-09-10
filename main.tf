b437ef195fcc08dd772cbeed6b657d15df230b1c
spf goa files <jkarlapalam@gmail.com>
2024-09-10 22:24:07 +0530

    fc 2 main.tf file

---
+++ main.tf
@@ -0,0 +1,22 @@
+# Set the Azure Provider source and version being used
+terraform {
+  required_version = ">= 0.14"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a Resource Group if it doesn’t exist
+resource "azurerm_resource_group" "tfexample" {
+  name     = "bw-tf-rg"
+  location = "West Europe"
+}
